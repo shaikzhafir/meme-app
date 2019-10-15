6b16c137ca0b15a7bf46eafe1bcfb334fa5168a0
bobross96 <zadus96@gmail.com>
2019-10-16 02:20:07 +0800

    added text preview function

--- index.js
+++ index.js
@@ -57,7 +57,6 @@ async function getMeme(){
             if (memeLower.includes(input)){
                 console.log(memes[i].url);
                 var imageUrl = memes[i].url;
-                this.imageUrl = imageUrl;
                 var image = document.createElement('img');
                 image.setAttribute('src',imageUrl);
                 image.setAttribute('id', memes[i].id);
@@ -70,20 +69,15 @@ async function getMeme(){
             }
         }
         
-
-
-
                  if (image == undefined){
                 var noImage = document.createElement('h1');
                 noImage.textContent = "how did u manage to yield zero results?";
                 section.appendChild(noImage);
             }
         
-
     })
     return memes;
-    /* makeList(memes);
-    console.log(memelist); */
+  
 }
 
 getMeme();
@@ -99,9 +93,64 @@ function getRand(){
 
 function createTextArea(){
     
-        var txtAreaHTML = "<div contentEditable='true' class='meme-txt-area'></div>";
+        var txtAreaHTML = "<div id='test' contentEditable='true' class='meme-txt-area'></div>";
         $("#selected-meme").append(txtAreaHTML);
-        $(".meme-txt-area").draggable();
+        $(".meme-txt-area").draggable({containment : "parent"});
         $(".meme-txt-area").focus();
     
 }
+
+function deleteText(){
+    $("#test").remove();
+
+}
+
+
+function copyToCanvas(htmlElement)
+{
+	var canvas = document.getElementById("meme-preview");
+    var ctx = canvas.getContext("2d");
+
+ 	image = new Image(0, 0);
+    	image.onload = function () {
+    		canvas.width = this.naturalWidth;
+    	    canvas.height = this.naturalHeight;
+        
+              ctx.drawImage(this, 0, 0);
+              
+              function drawStroked(text, left, top){
+                ctx.font = '2.5em Impact';
+                ctx.strokeStyle = 'black';
+                ctx.lineWidth = 4;
+                ctx.strokeText(text, left, top);
+                ctx.fillStyle = 'white';
+                ctx.fillText(text, left, top);
+              }
+              
+              var top = 0;
+              var left = 0;
+              var cellspace = 0; 
+    	      $(".meme-txt-area").each(function(){
+               cellspace = parseInt($(this).css("padding"));
+               left = parseInt($(this).css("left")) + cellspace;
+               top = parseInt($(this).css("top")) + 5 * cellspace;
+              let text = $(this).text().toUpperCase();
+              
+              drawStroked(text,left,top);
+              
+    	      });
+        };
+        
+        image.src = $(".image-fluid").attr("src");
+    }
+
+
+    $("#save-as-image").on('click', function () {
+        copyToCanvas($('.image-fluid'));
+   });
+
+   $("#save-file-btn").on('click', function () {
+    var canvas = document.getElementById("meme-preview");
+    var imageURL = canvas.toDataURL();
+    $("#download-image").attr("href", imageURL);
+});
