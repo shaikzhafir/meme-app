edc244570912a0a45271ab3c880e6c492819bf86
bobross96 <zadus96@gmail.com>
2020-12-02 18:57:12 +0800

    multi-box variation withthe shitty add text jquery code

--- index.js
+++ index.js
@@ -1,156 +1,194 @@
-
-
-var displayedImage = document.querySelector('.image-fluid');
-var searchBar = document.querySelector('input');
-var searchBtn = document.querySelector('#searchBtn');
-var main = document.querySelector('main');
-var section = document.createElement('section');
-var draggable = document.querySelector('.container');
-var addText = document.querySelector('#addText');
-
-main.appendChild(section);
+var displayedImage = document.querySelector(".current-meme");
+var searchBar = document.querySelector("input");
+var searchBtn = document.querySelector("#searchBtn");
+var main = document.querySelector(".fetched-memes");
+var draggable = document.querySelector(".container");
+var addText = document.querySelector("#addText");
+var boxNum = document.querySelector(".boxNum");
+var rightCol = document.querySelector(".rightCol");
 
 const MEME_API = "https://api.imgflip.com/get_memes";
 
-//dragndrop 
+//dragndrop
 
 /* $( function() {
     $( ".meme-txt-area" ).draggable( );
   } );
  */
 
-
 let memelist = [];
 
-function makeList(memes){
-
-for (i = 0; i < 100 ; i++){
-    memelist.push(memes[i].name);
+function makeList(memes) {
+  memes.forEach((meme) => {
+    memelist.push(meme.name);
+  });
 }
-}
 
-
-async function getMeme(){
-    const response = await fetch("https://api.imgflip.com/get_memes");
-    const data = await response.json();
-    const memes = data.data.memes;
-    /* displayedImage.onclick = (e => {
+async function getMeme() {
+  const response = await fetch("https://api.imgflip.com/get_memes");
+  const data = await response.json();
+  const memes = data.data.memes;
+  /* displayedImage.onclick = (e => {
         
         var imgSrc = memes[getRand()].url ;
         displayedImage.setAttribute('src', imgSrc);
     }) */
 
+  searchBtn.onclick = (e) => {
+    //make sure the memelist is not duplicated
+    if (memelist.length == 0) {
+      makeList(memes);
+    }
+    clearSection();
+    //image.setAttribute('src', null);
+    //memelist is array of meme names
+    //let input = searchBar.value.toLowerCase();
+    memes.forEach((meme) => {
+      var imageUrl = meme.url;
+      var image = document.createElement("img");
 
+      image.setAttribute("src", imageUrl);
+      image.setAttribute("id", meme.id);
+      image.setAttribute("class", "img-fluid");
+      image.setAttribute("alt", meme.box_count);
+      main.appendChild(image);
 
-    
-    searchBtn.onclick = (e => {
-        //make sure the memelist is not duplicated
-        if (memelist.length == 0){
-            makeList(memes);
-        }
-        clearSection();
-        //image.setAttribute('src', null);
-        //memelist is array of meme names
-        let input = searchBar.value.toLowerCase();
-        for ( i = 0 ; i < 100 ; i ++){
-            let memeLower = memelist[i].toLowerCase();
-            if (memeLower.includes(input)){
-                console.log(memes[i].url);
-                var imageUrl = memes[i].url;
-                var image = document.createElement('img');
-                image.setAttribute('src',imageUrl);
-                image.setAttribute('id', memes[i].id);
-                image.setAttribute('class','img-fluid');
-                section.appendChild(image);
-                document.getElementById(memes[i].id).onclick = (e =>{
-                    displayedImage.setAttribute('src', e.target.src);
-                })
-                
-            }
-        }
+      //set up function for every meme when clicked
+      document.getElementById(meme.id).onclick = (e) => {
+        displayedImage.setAttribute("src", e.target.src);
+        displayedImage.setAttribute("id", e.target.id);
+        displayedImage.setAttribute("class", "current-meme");
+        displayedImage.setAttribute("alt", e.target.alt);
+        //removes all previously added text boxes
+        $(".meme-txt-area").remove();
         
-                 if (image == undefined){
-                var noImage = document.createElement('h1');
-                noImage.textContent = "how did u manage to yield zero results?";
-                section.appendChild(noImage);
-            }
-        
-    })
-    return memes;
-  
+      };
+    });
+  };
 }
+
+async function captionMeme() {
+  const formData = new FormData();
+  let currentMeme = document.getElementsByClassName("current-meme");
+  let memeID = currentMeme[0].id;
+  let topText = document.getElementById("topText").value;
+  let botText = document.getElementById("botText").value;
+  //code for if boxes worked by post
+  /* let textboxes = document.querySelectorAll('.textboxes')
+  let boxes = [];
+  textboxes.forEach(textbox => {
+      let data = {
+          "text" : textbox.value
+      }
+      boxes.push(data);
+  }); */
+  //this should be done server side
+  formData.append("template_id", memeID);
+  formData.append("username", "bobross96");
+  formData.append("password", "Mememachine1996");
+  formData.append("text0", topText);
+  formData.append("text1", botText);
+
+  //formData.append("boxes",JSON.stringify([{"text":"test"}]));
+  console.log(formData);
+  fetch("https://api.imgflip.com/caption_image", {
+    method: "POST",
+    body: formData,
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      console.log("Success:", result);
+      displayedImage.setAttribute("src", result.data.url);
+      
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+const data = {
+  template_id: "438680",
+  username: "bobross96",
+  password: "Mememachine1996",
+  text0: "lala",
+  text1: "poop",
+};
 
 getMeme();
 
-function clearSection(){
-    document.querySelector('section').innerHTML = "";
+function clearSection() {
+  document.querySelector(".fetched-memes").innerHTML = "";
 }
 
-function getRand(){
-    return Math.floor(Math.random()*100);
-
+function getRand() {
+  return Math.floor(Math.random() * 100);
 }
 
-function createTextArea(){
-    
-        var txtAreaHTML = "<div id='test' contentEditable='true' class='meme-txt-area'></div>";
-        $("#selected-meme").append(txtAreaHTML);
-        $(".meme-txt-area").draggable({containment : "parent"});
-        $(".meme-txt-area").focus();
-    
+function createTextArea() {
+  var txtAreaHTML =
+    "<div id='textbox' contentEditable='true' class='meme-txt-area'></div>";
+  $("#selected-meme").append(txtAreaHTML);
+  $(".meme-txt-area").draggable({ containment: "parent" });
+  $(".meme-txt-area").focus();
 }
 
-function deleteText(){
-    $("#test").remove();
-
+function deleteText() {
+  $("#textbox").remove();
 }
 
+function copyToCanvas(htmlElement) {
+  var canvas = document.getElementById("meme-preview");
+  var ctx = canvas.getContext("2d");
 
-function copyToCanvas(htmlElement)
-{
-	var canvas = document.getElementById("meme-preview");
-    var ctx = canvas.getContext("2d");
+  image = new Image(0, 0);
+  image.onload = function () {
+    canvas.width = this.naturalWidth;
+    canvas.height = this.naturalHeight;
 
- 	image = new Image(0, 0);
-    	image.onload = function () {
-    		canvas.width = this.naturalWidth;
-    	    canvas.height = this.naturalHeight;
-        
-              ctx.drawImage(this, 0, 0);
-              
-              function drawStroked(text, left, top){
-                ctx.font = '2.5em Impact';
-                ctx.strokeStyle = 'black';
-                ctx.lineWidth = 4;
-                ctx.strokeText(text, left, top);
-                ctx.fillStyle = 'white';
-                ctx.fillText(text, left, top);
-              }
-              
-              var top = 0;
-              var left = 0;
-              var cellspace = 0; 
-    	      $(".meme-txt-area").each(function(){
-               cellspace = parseInt($(this).css("padding"));
-               left = parseInt($(this).css("left")) + cellspace;
-               top = parseInt($(this).css("top")) + 5 * cellspace;
-              let text = $(this).text().toUpperCase();
-              
-              drawStroked(text,left,top);
-              
-    	      });
-        };
-        
-        image.src = $(".image-fluid").attr("src");
+    ctx.drawImage(this, 0, 0);
+
+    function drawStroked(text, left, top) {
+      ctx.font = "2.5em Impact";
+      ctx.strokeStyle = "black";
+      ctx.lineWidth = 4;
+      ctx.strokeText(text, left, top);
+      ctx.fillStyle = "white";
+      ctx.fillText(text, left, top);
     }
 
+    var top = 0;
+    var left = 0;
+    var cellspace = 0;
+    //will draw text from current meme image to canvas
+    /* let textboxes = document.querySelectorAll(".meme-txt-area");
 
-    $("#save-as-image").on('click', function () {
-        copyToCanvas($('.image-fluid'));
-   });
+    
+    textboxes.forEach(box => {
+      cellspace = parseInt($(box).css("padding"));
+      left = parseInt($(box).css("left")) + cellspace;
+      top = parseInt($(box).css("top")) + 5 * cellspace;
+      let text = $(box).text().toUpperCase();
+      drawStroked(text,left,top);
+    }); */
+    
+    
+    jQuery.each($(".meme-txt-area"),function () {
+      console.log('im running');
+      cellspace = parseInt($(this).css("padding"));
+      left = parseInt($(this).css("left")) + cellspace;
+      top = parseInt($(this).css("top")) + 5 * cellspace;
+      let text = $(this).text().toUpperCase();
 
-   $("#save-file-btn").on('click', function () {
-    var canvas = document.getElementById("meme-preview");
-    var imageURL = canvas.toDataURL();
-    $("#download-image").attr("href", imageURL);
+      drawStroked(text, left, top);
+    });
+  };
+
+  image.src = $(".current-meme").attr("src");
+}
+
+$("#save-as-image").on("click", function () {
+  copyToCanvas($(".current-meme"));
+  /* var canvas = document.getElementById("meme-preview");
+  var imageURL = canvas.toDataURL();
+  $("#download-image").attr("href", imageURL); */
 });
